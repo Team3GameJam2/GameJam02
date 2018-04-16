36fdfb9d00b127d04139468d9d458fdab51c7c78
AIE\s171699 <adrianpeds15@gmail.com>
2018-04-16 14:46:15 +1000

    Adrians scene + scripts

---
+++ GameJam02/Assets/__Scripts/DeadZonEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(DeadzoneCamera))]
+public class DeadZonEditor : Editor
+{
+    public void OnSceneGUI()
+    {
+        DeadzoneCamera cam = target as DeadzoneCamera;
+        
+
+        Vector3[] vert =
+        {
+            cam.transform.position + new Vector3(cam.deadzone.xMin, cam.deadzone.yMin, 0),
+            cam.transform.position + new Vector3(cam.deadzone.xMax, cam.deadzone.yMin, 0),
+            cam.transform.position + new Vector3(cam.deadzone.xMax, cam.deadzone.yMax, 0),
+            cam.transform.position + new Vector3(cam.deadzone.xMin, cam.deadzone.yMax, 0)
+        };
+
+        Color transp = new Color(0, 0, 0, 0);
+        Handles.DrawSolidRectangleWithOutline(vert, transp, Color.red);
+
+        for (int i = 0; i<cam.limits.Length; ++i)
+        {
+            Vector3[] vertLimit =
+           {
+                new Vector3(cam.limits[i].xMin, cam.limits[i].yMin, 0),
+                new Vector3(cam.limits[i].xMax, cam.limits[i].yMin, 0),
+                new Vector3(cam.limits[i].xMax, cam.limits[i].yMax, 0),
+                new Vector3(cam.limits[i].xMin, cam.limits[i].yMax, 0)
+            };
+
+            Handles.DrawSolidRectangleWithOutline(vertLimit, transp, Color.green);
+        }
+    }
+}
---
+++ GameJam02/Assets/__Scripts/_Movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Movement : MonoBehaviour
+{
+    //Wisp RigidBody
+    Rigidbody Wisp;
+    //Speed
+    public float speed;
+    //Clamped Speed
+    public float maxSpeed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Wisps rigidbody
+        Wisp = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MoveLeft();
+
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+
+            MoveRight();
+        }
+
+        //Clamp for Max Velocity
+        Wisp.velocity = new Vector3(Mathf.Clamp(Wisp.velocity.x, -maxSpeed, maxSpeed), Wisp.velocity.y);
+    }
+    
+    public void MoveLeft()
+    {
+        //Add Force Left
+        Wisp.AddForce(-Wisp.transform.right* speed);
+    }
+
+    public void MoveRight()
+    {
+        //Add Force Right
+        Debug.Log("Im WORKING");
+        Wisp.AddForce(Wisp.transform.right* speed);
+    }
+
+    public void MoveUp()
+    {
+        Wisp.AddForce(Wisp.transform.up* speed);
+    }
+}
