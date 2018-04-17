2a6d95f3835f2d1e5682eba73f852c0c26fb55c2
AIE\s171699 <adrianpeds15@gmail.com>
2018-04-18 09:42:49 +1000

    Sound Script Adrian

---
+++ GameJam02/Assets/_Sound.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Sound : MonoBehaviour {
+
+    public Collider playerCol;
+    public AudioClip[] ImpactSound;
+    private AudioSource audio;
+
+    private bool iscoll = false;
+
+	// Use this for initialization
+	void Start () {
+
+        playerCol = GetComponent<Collider>();
+        audio = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other = playerCol;
+        if (iscoll == true)
+        {
+            audio.PlayOneShot(ImpactSound[Random.Range(0,1)]);
+        }
+        
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other = playerCol;
+        iscoll = true;
+    }
+
+}
